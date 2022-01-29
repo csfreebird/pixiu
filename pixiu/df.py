5f575ca6a08259f648c1f838c1c6f13c6982d8e5
Dean Chen <csfreebird@gmail.com>
2022-01-29 15:24:15 +0800

    add df.py

---
+++ pixiu/df.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def print_full(x):
+    pd.set_option('display.max_rows', len(x))
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 2000)
+    pd.set_option('display.float_format', '{:20,.2f}'.format)
+    pd.set_option('display.max_colwidth', None)
+    print(x)
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.width')
+    pd.reset_option('display.float_format')
+    pd.reset_option('display.max_colwidth')
